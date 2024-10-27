3c2663af96550a9192423d33fd4cd5ed7eb71f86
Serge Iangicher <sergey.yangicher@rockyroad.io>
2024-10-27 21:35:14 +0200

    hw01

---
+++ hw01.py
@@ -0,0 +1,24 @@
+def caching_fibonacci(number):
+    """The Fibonacci sequence is the series of numbers where each number is the sum of the two preceding numbers."""
+    cache = {}
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        
+        if n == 1:
+            return 1
+        
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci(number)
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
